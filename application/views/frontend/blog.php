70ddfac519101759352779a40b07a1d1cc1203ce
Miftah Aris Setiawan <miftahariss15@gmail.com>
2016-05-11 23:34:40 +0700

    initial commit

--- application/views/frontend/blog.php
+++ application/views/frontend/blog.php
@@ -1,9 +1,10 @@
 <div class="section">
 	<div class="container">
-        <div class="breadcrumb-wrap">
+        <!-- <div class="breadcrumb-wrap">
             <a href="index.html" class="breadcrumb">Home</a>
             <a href="blog.html" class="breadcrumb">Blog</a>
-        </div>
+        </div> -->
+        <?php echo $this->breadcrumbs->show(); ?>
     </div>
 </div> <!-- SECTION -->
 
@@ -14,105 +15,27 @@
         </div>
         
         <div class="blog-wrap">	
-        	<div class="article-box wow bounceInRight">
-            	<div class="article-img" style="background-image: url('<?php echo base_url(); ?>assets/images/article-img.jpg');">
-                	<!-- IMAGE AMBIL DARI BACKGROUND -->
-                </div>
-                
-                <a href="blog-detail.html" class="article-title">
-                	Lorem Ipsum Doret Amet Lorem Ipsum Doret Amet
-                </a>
-                
-                <p>
-                	Lorem ipsum doret amet maket hakaj kasdm mas john has never be told you everyting...
-                </p>
-                
-                <a href="blog-detail.html">Read More</a>
-            </div> <!-- ARTICLE BOX -->
+            <?php if ($blog > 0) :?>
+                <?php foreach($blog as $item): ?>
+                	<div class="article-box wow bounceInRight">
+                    	<div class="article-img" style="background-image: url('<?php echo base_url() ?>asset_admin/assets/uploads/cover/original/<?php echo $item['filename']; ?>');">
+                        	<!-- IMAGE AMBIL DARI BACKGROUND -->
+                        </div>
+                        
+                        <a href="<?php echo base_url(); ?>blog/<?php echo $item['permalink']; ?>" class="article-title">
+                        	<?php echo $item['title']; ?>
+                        </a>
+                        
+                        <p>
+                        	<?php echo $item['short_desc']; ?>
+                        </p>
+                        
+                        <a href="<?php echo base_url(); ?>blog/<?php echo $item['permalink']; ?>">Read More</a>
+                    </div> <!-- ARTICLE BOX -->
+                <?php endforeach; ?>
+            <?php endif; ?>
             
-            <div class="article-box wow bounceInRight" data-wow-delay="400ms">
-            	<div class="article-img" style="background-image: url('<?php echo base_url(); ?>assets/images/article-img.jpg');">
-                	<img class="play" src="<?php echo base_url(); ?>assets/images/play.png"> <!-- JIKA ARTICLE BERUPA VIDEO -->
-                    <!-- IMAGE AMBIL DARI BACKGROUND -->
-                </div>
-                
-                <a href="blog-detail.html" class="article-title">
-                	Lorem Ipsum Doret Amet Lorem Ipsum Doret Amet
-                </a>
-                
-                <p>
-                	Lorem ipsum doret amet maket hakaj kasdm mas john has never be told you everyting...
-                </p>
-                
-                <a href="blog-detail.html">Read More</a>
-            </div> <!-- ARTICLE BOX -->
-            
-            <div class="article-box wow bounceInRight" data-wow-delay="800ms">
-            	<div class="article-img" style="background-image: url('<?php echo base_url(); ?>assets/images/article-img.jpg');">
-                	<img class="play" src="<?php echo base_url(); ?>assets/images/play.png"> <!-- JIKA ARTICLE BERUPA VIDEO -->
-                    <!-- IMAGE AMBIL DARI BACKGROUND -->
-                </div>
-                
-                <a href="blog-detail.html" class="article-title">
-                	Lorem Ipsum Doret Amet Lorem Ipsum Doret Amet
-                </a>
-                
-                <p>
-                	Lorem ipsum doret amet maket hakaj kasdm mas john has never be told you everyting...
-                </p>
-                
-                <a href="blog-detail.html">Read More</a>
-            </div> <!-- ARTICLE BOX -->
-            
-            <div class="article-box wow bounceInRight">
-            	<div class="article-img" style="background-image: url('<?php echo base_url(); ?>assets/images/article-img.jpg');">
-                	<!-- IMAGE AMBIL DARI BACKGROUND -->
-                </div>
-                
-                <a href="blog-detail.html" class="article-title">
-                	Lorem Ipsum Doret Amet Lorem Ipsum Doret Amet
-                </a>
-                
-                <p>
-                	Lorem ipsum doret amet maket hakaj kasdm mas john has never be told you everyting...
-                </p>
-                
-                <a href="blog-detail.html">Read More</a>
-            </div> <!-- ARTICLE BOX -->
-            
-            <div class="article-box wow bounceInRight" data-wow-delay="400ms">
-            	<div class="article-img" style="background-image: url('<?php echo base_url(); ?>assets/images/article-img.jpg');">
-                	<!-- IMAGE AMBIL DARI BACKGROUND -->
-                </div>
-                
-                <a href="blog-detail.html" class="article-title">
-                	Lorem Ipsum Doret Amet Lorem Ipsum Doret Amet
-                </a>
-                
-                <p>
-                	Lorem ipsum doret amet maket hakaj kasdm mas john has never be told you everyting...
-                </p>
-                
-                <a href="blog-detail.html">Read More</a>
-            </div> <!-- ARTICLE BOX -->
-            
-            <div class="article-box wow bounceInRight" data-wow-delay="800ms">
-            	<div class="article-img" style="background-image: url('<?php echo base_url(); ?>assets/images/article-img.jpg');">
-                	<!-- IMAGE AMBIL DARI BACKGROUND -->
-                </div>
-                
-                <a href="blog-detail.html" class="article-title">
-                	Lorem Ipsum Doret Amet Lorem Ipsum Doret Amet
-                </a>
-                
-                <p>
-                	Lorem ipsum doret amet maket hakaj kasdm mas john has never be told you everyting...
-                </p>
-                
-                <a href="blog-detail.html">Read More</a>
-            </div> <!-- ARTICLE BOX -->
-            
-            <ul class="pagination">
+            <!-- <ul class="pagination">
                 <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
                 <li class="active"><a href="#!">1</a></li>
                 <li class=""><a href="#!">2</a></li>
@@ -120,7 +43,8 @@
                 <li><a href="#!">4</a></li>
                 <li><a href="#!">5</a></li>
                 <li><a href="#!"><i class="material-icons">chevron_right</i></a></li>
-            </ul>
+            </ul> -->
+            <?php echo $page_links; ?>
         </div> <!-- BLOG WRAP -->
     </div> <!-- CONTAINER -->
 </div> <!-- SECTION -->
