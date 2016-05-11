3b1b9c4acae746f0b08b2a0b78a5dcbf72524c6b
Miftah Aris Setiawan <miftahariss15@gmail.com>
2016-05-11 18:21:40 +0700

    initial commit

---
+++ application/views/frontend/about.php
@@ -0,0 +1,51 @@
+<div class="section">
+	<div class="container">
+        <div class="breadcrumb-wrap">
+            <a href="index.html" class="breadcrumb">Home</a>
+            <a href="about.html" class="breadcrumb">About</a>
+        </div>
+    </div>
+</div> <!-- SECTION -->
+
+<div class="section">
+	<div class="container">
+    	<div class="section-title">
+        	<h2>About Bisa Renang</h2>
+        </div>
+        
+        <div class="about">
+        	<h5>Mengenal Bisarenang</h5>
+            
+            <p>
+            	Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+            
+            <h5>Apa Tujuan Berenang</h5>
+            
+            <p>
+            	Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.                     
+            </p>
+            
+            <ul>
+                <li>Sehat</li>
+                <li>Semakin Tinggi</li>
+                <li>blablabla</li>
+                <li>Menyelam</li>
+            </ul>
+                
+            <h5>Mengapa Berenang</h5>
+            
+            <p>
+            	Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+            
+            <p>
+            	Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+            
+            <p>
+            	Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+        </div> <!-- ABOUT -->
+    </div> <!-- CONTAINER -->
+</div> <!-- SECTION -->
