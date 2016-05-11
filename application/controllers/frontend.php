3601e4b91045b356c4b4ac20e2efa22f98958c37
Miftah Aris Setiawan <miftahariss15@gmail.com>
2016-05-11 10:54:57 +0700

    initial commit

--- application/controllers/frontend.php
+++ application/controllers/frontend.php
@@ -15,4 +15,18 @@ class Frontend extends CI_Controller {
     	$data['mainpage'] = 'frontend/home';
     	$this->load->view('frontend/templates', $data);
     }
+
+    function blog(){
+        $data['base'] = 'Blog';
+
+        $data['mainpage'] = 'frontend/blog';
+        $this->load->view('frontend/templates', $data);
+    }
+
+    function safetyswim(){
+        $data['base'] = 'Safetyswim';
+
+        $data['mainpage'] = 'frontend/safetyswim';
+        $this->load->view('frontend/templates', $data);
+    }
 }
