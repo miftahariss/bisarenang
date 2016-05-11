41df9ccb262cd21b766f16ad810b1d672e0ea30d
Miftah Aris Setiawan <miftahariss15@gmail.com>
2016-05-11 18:22:15 +0700

    initial commit

---
+++ application/views/frontend/program.php
@@ -0,0 +1,36 @@
+<div class="section">
+	<div class="container">
+        <div class="breadcrumb-wrap">
+            <a href="index.html" class="breadcrumb">Home</a>
+            <a href="program.html" class="breadcrumb">Program</a>
+        </div>
+    </div>
+</div> <!-- SECTION -->
+
+<div class="section">
+	<div class="container">
+    	<div class="section-title">
+        	<h2>Program</h2>
+        </div>
+        
+        <div class="program-wrap  valign-wrapper wow fadeInUp" data-wow-duration="1s">
+            <a href="program-detail.html">
+                <img class="valign" src="<?php echo base_url(); ?>assets/images/program-1.jpg" alt="Program-1">
+                
+                <div class="program-caption">
+                    <h3>Basic to Swim</h3>
+                </div>
+            </a>
+        </div> <!-- PROGRAM WRAP -->
+        
+        <div class="program-wrap valign-wrapper wow fadeInUp" data-wow-duration="1s">
+            <a href="program-detail.html">
+                <img class="valign" src="<?php echo base_url(); ?>assets/images/program-2.jpg" alt="Program-2">
+                
+                <div class="program-caption">
+                    <h3>Swimming Fit</h3>
+                </div>
+            </a>
+        </div> <!-- PROGRAM WRAP -->
+    </div> <!-- CONTAINER -->
+</div> <!-- SECTION -->
