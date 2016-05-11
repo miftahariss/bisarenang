1eaf9bb6ac20a453f5157afa5c99f3a4f96348fb
Miftah Aris Setiawan <miftahariss15@gmail.com>
2016-05-11 18:19:24 +0700

    initial commit

---
+++ application/views/frontend/safetyswim.php
@@ -0,0 +1,64 @@
+<div class="section">
+	<div class="container">
+        <div class="breadcrumb-wrap">
+            <a href="index.html" class="breadcrumb">Home</a>
+            <a href="safetyswim.html" class="breadcrumb">Safety Swim</a>
+        </div>
+    </div>
+</div> <!-- SECTION -->
+
+<div class="section">
+	<div class="container">
+    	<div class="section-title">
+        	<h2>Safety Swim</h2>
+        </div>
+        
+        <div class="safetyswim-box type-a wow fadeInRightBig">
+            <div class="safetyswim-img" style="background-image: url('<?php echo base_url(); ?>assets/images/article-img.jpg');">
+                <!-- AMIBIL IMAGE DARI BACKGROUND -->
+            </div>
+            
+            <p>
+                Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas
+            </p>
+            
+            <div class="safetyswim-line"></div>
+        </div> <!-- SAFETY SWIM BOX TYPE A -->
+        
+        <div class="safetyswim-box type-b wow fadeInLeftBig">
+            <div class="safetyswim-img" style="background-image: url('<?php echo base_url(); ?>assets/images/article-img.jpg');">
+                <!-- AMIBIL IMAGE DARI BACKGROUND -->
+            </div>
+            
+            <p>
+                Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas
+            </p>
+            
+            <div class="safetyswim-line"></div>
+        </div> <!-- SAFETY SWIM BOX TYPE B -->
+        
+        <div class="safetyswim-box type-a wow fadeInRightBig">
+            <div class="safetyswim-img" style="background-image: url('<?php echo base_url(); ?>assets/images/article-img.jpg');">
+                <!-- AMIBIL IMAGE DARI BACKGROUND -->
+            </div>
+            
+            <p>
+                Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas
+            </p>
+            
+            <div class="safetyswim-line"></div>
+        </div> <!-- SAFETY SWIM BOX TYPE A -->
+        
+        <div class="safetyswim-box type-b wow fadeInLeftBig">
+            <div class="safetyswim-img" style="background-image: url('<?php echo base_url(); ?>assets/images/article-img.jpg');">
+                <!-- AMIBIL IMAGE DARI BACKGROUND -->
+            </div>
+            
+            <p>
+                Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas
+            </p>
+            
+            <div class="safetyswim-line"></div>
+        </div> <!-- SAFETY SWIM BOX TYPE B -->
+    </div> <!-- CONTAINER -->
+</div> <!-- SECTION -->
