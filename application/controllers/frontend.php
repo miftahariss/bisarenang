c448ec584b27d63417fea5dcf3a3b394448212a1
Miftah Aris Setiawan <miftahariss15@gmail.com>
2016-05-11 23:33:41 +0700

    initial commit

--- application/controllers/frontend.php
+++ application/controllers/frontend.php
@@ -7,7 +7,7 @@ class Frontend extends CI_Controller {
 	function __construct() {
         parent::__construct();
         $this->load->model('m_frontend');
-        $this->load->library('breadcrumb');
+        $this->load->library('breadcrumbs');
     }
 
     function index(){
@@ -30,6 +30,52 @@ class Frontend extends CI_Controller {
     function blog(){
         $data['base'] = 'Blog';
 
+        $this->breadcrumbs->push('Home', '/');
+        $this->breadcrumbs->push('Blog', '/blog');
+
+        // unshift crumb
+        //$this->breadcrumbs->unshift('Home', '/');
+
+        $limit = 6;
+
+        $this->db->where_in('status', array('1'));
+        $this->db->order_by("created_date", "desc");
+        $this->db->limit($limit, $this->uri->segment(2));
+        $blog_query = $this->db->get('blog');
+        $data['blog'] = $blog_query->result_array();
+
+        $this->db->where_in('status', array('1'));
+        $query_total = $this->db->get('blog');
+        $data['total'] = $query_total->num_rows();
+
+        $this->load->library('pagination');
+        $config['base_url'] = site_url('blog');
+        $config['total_rows'] = $data['total'];
+        $config['per_page'] = $limit;
+        $config['uri_segment'] = 2;
+        $config['num_links'] = 2;
+        $config['prev_link'] = '<i class="material-icons">chevron_left</i>';
+        $config['prev_tag_open'] = '<li>';
+        $config['prev_tag_close'] = '</li>';
+        $config['next_link'] = '<i class="material-icons">chevron_right</i>';
+        $config['next_tag_open'] = '<li>';
+        $config['next_tag_close'] = '</li>';
+        $config['last_link'] = '';
+        $config['last_tag_open'] = '';
+        $config['last_tag_close'] = '';
+        $config['first_link'] = '';
+        $config['first_tag_open'] = '';
+        $config['first_tag_close'] = '';
+        $config['num_tag_open'] = '<li>';
+        $config['num_tag_close'] = '</li>';
+        $config['cur_tag_open'] = '<li class="active">';
+        $config['cur_tag_close'] = '</li>';
+        $config['full_tag_open'] = '<ul class="pagination">';
+        $config['full_tag_close'] = '</ul>';
+
+        $this->pagination->initialize($config);
+        $data['page_links'] = $this->pagination->create_links();
+
         $data['mainpage'] = 'frontend/blog';
         $this->load->view('frontend/templates', $data);
     }
@@ -40,10 +86,17 @@ class Frontend extends CI_Controller {
         $title = $this->uri->segment(2);
 
         $data['content_detail'] = $this->m_frontend->getDetailBlog($title);
+        $content_detail = $data['content_detail'];
 
-        if ($data['content_detail'] == FALSE) {
-            redirect(show_404());
+        if ($content_detail == FALSE) {
+            redirect('pagenotfound');
         }
+
+        $this->breadcrumbs->push('Home', '/');
+        $this->breadcrumbs->push('Blog', '/blog');
+        $this->breadcrumbs->push($content_detail[0]->title, $content_detail[0]->permalink);
+
+        //var_dump($data['content_detail']);exit;
 
         $data['mainpage'] = 'frontend/blogdetail';
         $this->load->view('frontend/templates', $data);
@@ -89,5 +142,10 @@ class Frontend extends CI_Controller {
 
         $data['mainpage'] = 'frontend/about';
         $this->load->view('frontend/templates', $data);
+    }
+
+    function pagenotfound(){
+        $this->output->set_status_header('404'); // setting header to 404
+        $this->load->view('frontend/pagenotfound');//loading view
     }
 }
