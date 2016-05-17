280716d5a5291704cc4be461bca94fe4cc902a34
Miftah Aris Setiawan <miftahariss15@gmail.com>
2016-05-17 15:50:20 +0700

    initial commit

--- application/config/constants.php
+++ application/config/constants.php
@@ -36,6 +36,14 @@ define('FOPEN_READ_WRITE_CREATE',				'a+b');
 define('FOPEN_WRITE_CREATE_STRICT',				'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
+if($_SERVER['HTTP_HOST'] == "localhost") {
+	define('URL_EDITOR', 'http://localhost/bisarenang/asset_admin/assets/embed/');
+	define('DIR_EDITOR', '/Applications/XAMPP/xamppfiles/htdocs/bisarenang/asset_admin/assets/embed');
+} else {
+	define('URL_EDITOR', 'http://10.31.201.223/bisarenang/asset_admin/assets/embed/');
+	define('DIR_EDITOR', '/Applications/XAMPP/xamppfiles/htdocs/bisarenang/asset_admin/assets/embed');
+}
+
 
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
