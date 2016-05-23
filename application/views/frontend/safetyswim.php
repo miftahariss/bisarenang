e95dcce892b02cb1632710c0b613654f825a5e72
Miftah Aris Setiawan <miftahariss15@gmail.com>
2016-05-23 19:11:35 +0700

    initial commit

--- application/views/frontend/safetyswim.php
+++ application/views/frontend/safetyswim.php
@@ -1,64 +1,31 @@
 <div class="section">
-	<div class="container">
-        <div class="breadcrumb-wrap">
+    <div class="container">
+        <!-- <div class="breadcrumb-wrap">
             <a href="index.html" class="breadcrumb">Home</a>
             <a href="safetyswim.html" class="breadcrumb">Safety Swim</a>
-        </div>
+        </div> -->
+        <?php echo $this->breadcrumbs->show(); ?>
     </div>
 </div> <!-- SECTION -->
 
 <div class="section">
-	<div class="container">
-    	<div class="section-title">
-        	<h2>Safety Swim</h2>
+    <div class="container">
+        <div class="section-title wow fadeInDown">
+            <h2>Safety Swim</h2>
         </div>
-        
-        <div class="safetyswim-box type-a wow fadeInRightBig">
-            <div class="safetyswim-img" style="background-image: url('<?php echo base_url(); ?>assets/images/article-img.jpg');">
-                <!-- AMIBIL IMAGE DARI BACKGROUND -->
-            </div>
-            
-            <p>
-                Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas
-            </p>
-            
-            <div class="safetyswim-line"></div>
-        </div> <!-- SAFETY SWIM BOX TYPE A -->
-        
-        <div class="safetyswim-box type-b wow fadeInLeftBig">
-            <div class="safetyswim-img" style="background-image: url('<?php echo base_url(); ?>assets/images/article-img.jpg');">
-                <!-- AMIBIL IMAGE DARI BACKGROUND -->
-            </div>
-            
-            <p>
-                Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas
-            </p>
-            
-            <div class="safetyswim-line"></div>
-        </div> <!-- SAFETY SWIM BOX TYPE B -->
-        
-        <div class="safetyswim-box type-a wow fadeInRightBig">
-            <div class="safetyswim-img" style="background-image: url('<?php echo base_url(); ?>assets/images/article-img.jpg');">
-                <!-- AMIBIL IMAGE DARI BACKGROUND -->
-            </div>
-            
-            <p>
-                Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas
-            </p>
-            
-            <div class="safetyswim-line"></div>
-        </div> <!-- SAFETY SWIM BOX TYPE A -->
-        
-        <div class="safetyswim-box type-b wow fadeInLeftBig">
-            <div class="safetyswim-img" style="background-image: url('<?php echo base_url(); ?>assets/images/article-img.jpg');">
-                <!-- AMIBIL IMAGE DARI BACKGROUND -->
-            </div>
-            
-            <p>
-                Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas Lorem ipsum dorem doreams asklsajdalskdas
-            </p>
-            
-            <div class="safetyswim-line"></div>
-        </div> <!-- SAFETY SWIM BOX TYPE B -->
+
+        <?php if (count($content_safety) > 0) :?>
+            <?php foreach($content_safety as $data): ?>
+                <div class="safetyswim-box wow fadeInRightBig">
+                    <div class="safetyswim-img">
+                        <img src="<?php echo base_url() ?>asset_admin/assets/uploads/cover/original/<?php echo $data->filename; ?>" alt="<?php echo $data->title; ?>">
+                    </div>
+                    
+                    <p>
+                        <?php echo $data->short_desc; ?>
+                    </p>
+                </div> <!-- SAFETY SWIM BOX -->
+            <?php endforeach; ?>
+        <?php endif; ?>
     </div> <!-- CONTAINER -->
 </div> <!-- SECTION -->
