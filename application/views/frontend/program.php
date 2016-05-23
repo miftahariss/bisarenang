a852002d74206c6d45a4d7ef54a44317e73b935e
Miftah Aris Setiawan <miftahariss15@gmail.com>
2016-05-23 19:11:23 +0700

    initial commit

--- application/views/frontend/program.php
+++ application/views/frontend/program.php
@@ -1,36 +1,39 @@
 <div class="section">
-	<div class="container">
-        <div class="breadcrumb-wrap">
+    <div class="container">
+        <!-- <div class="breadcrumb-wrap">
             <a href="index.html" class="breadcrumb">Home</a>
             <a href="program.html" class="breadcrumb">Program</a>
-        </div>
+        </div> -->
+        <?php echo $this->breadcrumbs->show(); ?>
     </div>
 </div> <!-- SECTION -->
 
 <div class="section">
-	<div class="container">
-    	<div class="section-title">
-        	<h2>Program</h2>
+    <div class="container">
+        <div class="section-title wow fadeInDown">
+            <h2>Program</h2>
         </div>
         
-        <div class="program-wrap  valign-wrapper wow fadeInUp" data-wow-duration="1s">
-            <a href="program-detail.html">
-                <img class="valign" src="<?php echo base_url(); ?>assets/images/program-1.jpg" alt="Program-1">
-                
-                <div class="program-caption">
-                    <h3>Basic to Swim</h3>
-                </div>
-            </a>
-        </div> <!-- PROGRAM WRAP -->
-        
-        <div class="program-wrap valign-wrapper wow fadeInUp" data-wow-duration="1s">
-            <a href="program-detail.html">
-                <img class="valign" src="<?php echo base_url(); ?>assets/images/program-2.jpg" alt="Program-2">
-                
-                <div class="program-caption">
-                    <h3>Swimming Fit</h3>
-                </div>
-            </a>
-        </div> <!-- PROGRAM WRAP -->
+        <ul class="collapsible" data-collapsible="accordion">
+            <?php if (count($content_program) > 0) :?>
+                <?php $i = 1; ?>
+                <?php foreach($content_program as $data): ?>
+                    <li class="wow fadeInRight" data-wow-delay="<?php echo $i; ?>s"> <!-- DATA WOW DELAY KELIPATAN TAMBAH 1 -->
+                        <div class="collapsible-header" style="background-image: url(<?php echo base_url(); ?>assets/images/program-menu.jpg);">
+                            <p>Technique</p>
+                        </div>
+                        
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="#">Beginner</a></li>
+                                <li><a href="#">Intermediate</a></li>
+                                <li><a href="#">Advance</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <?php $i++; ?>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </ul> <!-- COLLAPSIBLE -->
     </div> <!-- CONTAINER -->
 </div> <!-- SECTION -->
