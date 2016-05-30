0be0623d5cd87a3737a616e81efd77fa1e8bc9a9
Miftah Aris Setiawan <miftahariss15@gmail.com>
2016-05-30 17:42:55 +0700

    initial commit

--- application/views/frontend/templates.php
+++ application/views/frontend/templates.php
@@ -11,7 +11,7 @@
         <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/slick-theme.css"/>
         <!--Let browser know website is optimized for mobile-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        
+        <title>Bisa Renang</title>
     </head>
     
     <body>
