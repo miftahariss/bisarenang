e68f93d01ce73475c73ba406f6731f21cc12040c
Miftah Aris Setiawan <miftahariss15@gmail.com>
2016-06-30 16:05:09 +0700

    initial commit

--- database/bisarenang.sql
+++ database/bisarenang.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
+-- version 4.0.10.14
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: 30 Mei 2016 pada 09.33
--- Versi Server: 10.1.10-MariaDB
--- PHP Version: 5.5.33
+-- Inang: localhost:3306
+-- Waktu pembuatan: 30 Jun 2016 pada 08.57
+-- Versi Server: 5.5.50-cll
+-- Versi PHP: 5.4.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,10 +14,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Database: `bisarenang`
+-- Basis data: `k8059924_bisarenang`
 --
 
 -- --------------------------------------------------------
@@ -26,8 +26,8 @@ SET time_zone = "+00:00";
 -- Struktur dari tabel `about`
 --
 
-CREATE TABLE `about` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `about` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_account` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
   `short_desc` text NOT NULL,
@@ -36,29 +36,24 @@ CREATE TABLE `about` (
   `modified_date` int(11) DEFAULT NULL,
   `created_by` tinyint(2) DEFAULT NULL,
   `modified_by` tinyint(2) DEFAULT NULL,
-  `status` tinyint(1) DEFAULT '1' COMMENT '0=inactive; 1=active; 2=draft; default=1'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `about`
---
-
-INSERT INTO `about` (`id`, `id_account`, `title`, `short_desc`, `body`, `created_date`, `modified_date`, `created_by`, `modified_by`, `status`) VALUES
-(1, 0, 'About Bisa Renang', 'About Bisa Renang', '<h5>Mengenal Bisarenang</h5>\n\n<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\n\n<h5>Apa Tujuan Berenang</h5>\n\n<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\n\n<ul>\n	<li>Sehat</li>\n	<li>Semakin Tinggi</li>\n	<li>blablabla</li>\n	<li>Menyelam</li>\n</ul>\n\n<h5>Mengapa Berenang</h5>\n\n<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\n\n<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\n\n<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\n', 1464591288, 1464591502, NULL, 1, 1);
+  `status` tinyint(1) DEFAULT '1' COMMENT '0=inactive; 1=active; 2=draft; default=1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `blog`
+-- Struktur dari tabel `basic`
 --
 
-CREATE TABLE `blog` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `basic` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_account` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
   `short_desc` tinytext NOT NULL,
   `body` text,
-  `filename` varchar(125) NOT NULL,
+  `filename` varchar(125) DEFAULT NULL,
+  `video_id` varchar(125) DEFAULT NULL,
   `headline` int(11) NOT NULL,
   `permalink` varchar(120) NOT NULL,
   `meta_keywords` varchar(255) NOT NULL,
@@ -67,16 +62,48 @@ CREATE TABLE `blog` (
   `modified_date` int(11) DEFAULT NULL,
   `created_by` tinyint(2) DEFAULT NULL,
   `modified_by` tinyint(2) DEFAULT NULL,
-  `status` tinyint(1) DEFAULT '1' COMMENT '0=inactive; 1=active; 2=draft; default=1'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `status` tinyint(1) DEFAULT '1' COMMENT '0=inactive; 1=active; 2=draft; default=1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data untuk tabel `blog`
+-- Struktur dari tabel `basic_counter`
 --
 
-INSERT INTO `blog` (`id`, `id_account`, `title`, `short_desc`, `body`, `filename`, `headline`, `permalink`, `meta_keywords`, `meta_description`, `created_date`, `modified_date`, `created_by`, `modified_by`, `status`) VALUES
-(1, 1, 'judul', 'short desc', '<p>isi</p>\n', '1462963112.jpg', 1, 'judul.html', '0', '0', 1462954408, 1464585305, 1, 1, 1),
-(2, 1, 'judul 2', 'short desc 2', '<p>isi</p>\n', '1462966260.jpg', 0, 'judul-2.html', 'meta keywords', 'meta description', 1462966260, 1463125098, 1, 1, 1);
+CREATE TABLE IF NOT EXISTS `basic_counter` (
+  `counter_basic_id` int(20) NOT NULL,
+  `counter_count` int(11) NOT NULL,
+  `counter_count_date` date NOT NULL,
+  PRIMARY KEY (`counter_basic_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `blog`
+--
+
+CREATE TABLE IF NOT EXISTS `blog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_account` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `short_desc` tinytext NOT NULL,
+  `body` text,
+  `filename` varchar(125) NOT NULL,
+  `video_id` varchar(125) NOT NULL,
+  `headline` int(11) NOT NULL,
+  `permalink` varchar(120) NOT NULL,
+  `meta_keywords` varchar(255) NOT NULL,
+  `meta_description` varchar(255) NOT NULL,
+  `created_date` int(11) NOT NULL,
+  `modified_date` int(11) DEFAULT NULL,
+  `created_by` tinyint(2) DEFAULT NULL,
+  `modified_by` tinyint(2) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT '1' COMMENT '0=inactive; 1=active; 2=draft; default=1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 -- --------------------------------------------------------
 
@@ -84,19 +111,12 @@ INSERT INTO `blog` (`id`, `id_account`, `title`, `short_desc`, `body`, `filename
 -- Struktur dari tabel `blog_counter`
 --
 
-CREATE TABLE `blog_counter` (
+CREATE TABLE IF NOT EXISTS `blog_counter` (
   `counter_blog_id` int(20) NOT NULL,
   `counter_count` int(11) NOT NULL,
-  `counter_count_date` date NOT NULL
+  `counter_count_date` date NOT NULL,
+  PRIMARY KEY (`counter_blog_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
---
--- Dumping data untuk tabel `blog_counter`
---
-
-INSERT INTO `blog_counter` (`counter_blog_id`, `counter_count`, `counter_count_date`) VALUES
-(2, 7, '2016-05-17'),
-(1, 5, '2016-05-30');
 
 -- --------------------------------------------------------
 
@@ -104,8 +124,8 @@ INSERT INTO `blog_counter` (`counter_blog_id`, `counter_count`, `counter_count_d
 -- Struktur dari tabel `program`
 --
 
-CREATE TABLE `program` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `program` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_account` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
   `short_desc` tinytext NOT NULL,
@@ -116,16 +136,9 @@ CREATE TABLE `program` (
   `modified_date` int(11) DEFAULT NULL,
   `created_by` tinyint(2) DEFAULT NULL,
   `modified_by` tinyint(2) DEFAULT NULL,
-  `status` tinyint(1) DEFAULT '1' COMMENT '0=inactive; 1=active; 2=draft; default=1'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `program`
---
-
-INSERT INTO `program` (`id`, `id_account`, `title`, `short_desc`, `filename`, `headline`, `permalink`, `created_date`, `modified_date`, `created_by`, `modified_by`, `status`) VALUES
-(4, 1, 'judul program 2', 'short desc 2', '1464070284.jpg', 0, 'judul-program-2.html', 1463395200, 1464070284, 1, 1, 1),
-(5, 1, 'judul program', 'short desc program', '1463470391.jpg', 1, 'judul-program.html', 1463470160, 1464584299, 1, 1, 1);
+  `status` tinyint(1) DEFAULT '1' COMMENT '0=inactive; 1=active; 2=draft; default=1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 -- --------------------------------------------------------
 
@@ -133,8 +146,8 @@ INSERT INTO `program` (`id`, `id_account`, `title`, `short_desc`, `filename`, `h
 -- Struktur dari tabel `program_level`
 --
 
-CREATE TABLE `program_level` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `program_level` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_program` int(11) NOT NULL,
   `level` int(11) NOT NULL,
   `id_account` int(11) NOT NULL,
@@ -149,20 +162,9 @@ CREATE TABLE `program_level` (
   `modified_date` int(11) DEFAULT NULL,
   `created_by` tinyint(2) DEFAULT NULL,
   `modified_by` tinyint(2) DEFAULT NULL,
-  `status` tinyint(1) DEFAULT '1' COMMENT '0=inactive; 1=active; 2=draft; default=1'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `program_level`
---
-
-INSERT INTO `program_level` (`id`, `id_program`, `level`, `id_account`, `title`, `short_desc`, `body`, `filename`, `permalink`, `meta_keywords`, `meta_description`, `created_date`, `modified_date`, `created_by`, `modified_by`, `status`) VALUES
-(7, 4, 1, 1, 'wewewe', 'hjhjhj', '<p>hjhjhjh</p>\n', '1464070258.jpg', 'wewewe.html', '', '', 1463395200, 1464070258, 1, 1, 1),
-(8, 4, 2, 1, 'xcxcxc', 'hjhjhjhjh', '<p>jhjhjhjhj</p>\n', '1464070269.jpg', 'xcxcxc.html', '', '', 1463395200, 1464070269, 1, 1, 1),
-(9, 4, 3, 1, 'ghghgh', 'khhjhjhj', '<p>jhjhjhjhj</p>\n', '1464070279.jpg', 'ghghgh.html', '', '', 1463395200, 1464070279, 1, 1, 1),
-(10, 5, 1, 1, 'judul beginner', 'short desc beginner', '<p>isi beginner</p>\n', '1463473914.jpg', 'judul-beginner.html', '', '', 1463470160, 1463473914, 1, 1, 1),
-(11, 5, 2, 1, 'judul intermediate', 'short desc intermediate', '<p>isi intermediate</p>\n', '1463474027.jpg', 'judul-intermediate.html', '', '', 1463470160, 1463474027, 1, 1, 1),
-(12, 5, 3, 1, 'judul advanced', 'short desc advanced', '<p>isi advanced</p>\n', '1463474048.jpg', 'judul-advanced.html', '', '', 1463470160, 1463474048, 1, 1, 1);
+  `status` tinyint(1) DEFAULT '1' COMMENT '0=inactive; 1=active; 2=draft; default=1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
 -- --------------------------------------------------------
 
@@ -170,8 +172,8 @@ INSERT INTO `program_level` (`id`, `id_program`, `level`, `id_account`, `title`,
 -- Struktur dari tabel `safety`
 --
 
-CREATE TABLE `safety` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `safety` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_account` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
   `short_desc` tinytext NOT NULL,
@@ -183,15 +185,9 @@ CREATE TABLE `safety` (
   `modified_date` int(11) DEFAULT NULL,
   `created_by` tinyint(2) DEFAULT NULL,
   `modified_by` tinyint(2) DEFAULT NULL,
-  `status` tinyint(1) DEFAULT '1' COMMENT '0=inactive; 1=active; 2=draft; default=1'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `safety`
---
-
-INSERT INTO `safety` (`id`, `id_account`, `title`, `short_desc`, `filename`, `permalink`, `meta_keywords`, `meta_description`, `created_date`, `modified_date`, `created_by`, `modified_by`, `status`) VALUES
-(1, 1, 'safety swim title 2', 'safety swim short desc 2', '1463996172.jpg', 'safety-swim-title-2.html', '', '', 1463995767, 1463996172, 1, 1, 1);
+  `status` tinyint(1) DEFAULT '1' COMMENT '0=inactive; 1=active; 2=draft; default=1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 -- --------------------------------------------------------
 
@@ -199,104 +195,18 @@ INSERT INTO `safety` (`id`, `id_account`, `title`, `short_desc`, `filename`, `pe
 -- Struktur dari tabel `user`
 --
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   `email` varchar(150) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
   `created_date` int(11) DEFAULT NULL,
   `modified_date` int(11) DEFAULT NULL,
   `status` int(1) DEFAULT '1',
-  `role` tinyint(1) DEFAULT NULL COMMENT '1: administrator; 2: editor'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `role` tinyint(1) DEFAULT NULL COMMENT '1: administrator; 2: editor',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
---
--- Dumping data untuk tabel `user`
---
-
-INSERT INTO `user` (`id`, `name`, `email`, `password`, `created_date`, `modified_date`, `status`, `role`) VALUES
-(1, 'bisarenang', 'admin@bisarenang.com', 'c47d75eea049ea5afdac759973cb8075f58ad922', 0, 1464591009, 1, 1);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `about`
---
-ALTER TABLE `about`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `blog`
---
-ALTER TABLE `blog`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `blog_counter`
---
-ALTER TABLE `blog_counter`
-  ADD PRIMARY KEY (`counter_blog_id`);
-
---
--- Indexes for table `program`
---
-ALTER TABLE `program`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `program_level`
---
-ALTER TABLE `program_level`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `safety`
---
-ALTER TABLE `safety`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `about`
---
-ALTER TABLE `about`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `blog`
---
-ALTER TABLE `blog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `program`
---
-ALTER TABLE `program`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
---
--- AUTO_INCREMENT for table `program_level`
---
-ALTER TABLE `program_level`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
---
--- AUTO_INCREMENT for table `safety`
---
-ALTER TABLE `safety`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
